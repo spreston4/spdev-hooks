0e6a9cc5960be426562fa564d8a502c2519d85d7
Sam Preston <sam.preston@headway.io>
2023-11-13 15:47:46 -0800

    useOnScreen

---
+++ src/hooks/useOnScreen.ts
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+interface UseOnScreenArgs {
+  ref: React.RefObject<HTMLElement>;
+  rootMargin?: string;
+}
+
+const useOnScreen = ({ ref, rootMargin = "0px" }: UseOnScreenArgs): boolean => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (ref.current == null) return;
+    const observer = new IntersectionObserver(
+      ([entry]) => setIsVisible(entry.isIntersecting),
+      { rootMargin }
+    );
+    observer.observe(ref.current);
+    return () => {
+      if (ref.current == null) return;
+      observer.unobserve(ref.current);
+    };
+  }, [ref.current, rootMargin]);
+  return isVisible;
+};
+
+export default useOnScreen;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useArray } from "./hooks/useArray";
 export { default as useCounter } from "./hooks/useCounter";
+export { default as useOnScreen } from "./hooks/useOnScreen";
 export { default as useToggle } from "./hooks/useToggle";
